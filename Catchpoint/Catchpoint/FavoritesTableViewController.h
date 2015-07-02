c2139a57c63e895f1e1017184de4cbda17e92d9f
Matias <matiseijas@gmail.com>
2015-07-02 12:29:27 -0400

    Implememted FavoritesTableViewController

---
+++ Catchpoint/Catchpoint/FavoritesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesTableViewController.h
+//  Catchpoint
+//
+//  Created by Matias on 7/2/15.
+//  Copyright (c) 2015 Catchpoint Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesTableViewController : UITableViewController
+
+@end
